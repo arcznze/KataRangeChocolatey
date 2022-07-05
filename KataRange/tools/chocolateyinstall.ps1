5bf1e30fd6bba04925db7236908808034c52149b
arcznze <87095931+arcznze@users.noreply.github.com>
2022-07-05 11:23:55 -0400

    Add files via upload

---
+++ KataRange/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$name = "katarange"
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$path = Join-Path $toolsDir "KataRangeApp.exe"
+
+Install-BinFile -name $name -path $path
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
