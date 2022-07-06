f50cfa1bbb5221436df08f25a8f6f3a4702f1e0c
arcznze <87095931+arcznze@users.noreply.github.com>
2022-07-05 22:05:54 -0400

    Delete KataRange directory

--- KataRange/tools/chocolateyinstall.ps1
+++
@@ -1,5 +0,0 @@
-﻿$name = "katarange"
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$path = Join-Path $toolsDir 'KataRangeApp.exe'
-
-Install-BinFile -name $name -path $path
